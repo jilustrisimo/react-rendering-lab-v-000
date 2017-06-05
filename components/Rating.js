5205110337601824f27847a1dc90968569d9329f
github username <jilustrisimo@users.noreply.github.com>
2017-06-04 23:14:23 -0400

    Done.

--- components/Rating.js
+++ components/Rating.js
@@ -2,6 +2,17 @@ import React from 'react';
 
 export default class Rating extends React.Component {
 
+  componentWillReceiveProps(nextProps) {
+    let upTrend = this.props.rating < nextProps.rating
+    let noChange = this.props.rating === nextProps.rating
+    if (noChange) { return null }
+    if (upTrend) {
+     this.setState({increasing: true, decreasing: false})
+   } else if (!upTrend) {
+      this.setState({increasing: false, decreasing: true})
+    }
+  }
+
   constructor(props) {
     super(props);
     this.state = {
